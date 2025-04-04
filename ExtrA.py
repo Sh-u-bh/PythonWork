2248b1c02597b6f69a373a43cf6ac4ec7377d61f
Sh-u-bh <shubham.nivzentech@gmail.com>
2025-04-04 17:42:41 +0530

    hi

---
+++ ExtrA.py
@@ -0,0 +1,4 @@
+letters = ["a","b","c"]
+matrix = [[0,1],[2,3]]
+zeros = [0] * 5
+combined = zeros + letters
--- py.py
+++ py.py
@@ -1,28 +1,12 @@
-'''
-print("Hello World")
-print("Hello World")
-count = 0
-for i in range (1,10):
-    if i % 2 == 0:
-        count+=1
-        print(i)
-print(f"We have {count} numbers")
-def greet(firstName , Lastname):
-    print(f"Hello {firstName} {Lastname}")
-    print("Shubham Here this side")
-
-greet("Shubham" , "Chandegara")
-
-def get_greet(name):
-    print (f"Hi {name}")
-
-print(get_greet("Shubham "))
-'''
-def multiply(*numbers):
-    total = 1
-    for number in numbers:
-        total *= number 
-        return total
-print ("Start")
-print (multiply(1, 2, 3))
-
+# def fizz_buzz(input):
+#     if (input % 3 == 0) and (input % 5 == 0):
+#         return "FizzBuzz"
+#     if input % 3 == 0:
+#         return "Fizz"
+#     if input % 5 == 0:
+#         return "Buzz"
+#     return input
+# print(fizz_buzz(15))
+letters = ["a","b","c"]
+matrix = [[0,1],[2,3]]
+zeros = [0] * 5
